c29059bbea6a9995609b8e389748407d4a86b3bc
tjark002 <tjarkh@arcor.de>
2021-09-09 12:42:47 +0200

    Create popupScriptTag.js

---
+++ popupScriptTag.js
@@ -0,0 +1,69 @@
+/*jQuery(document).ready(function() {
+
+		const path = window.location.pathname;
+	    // The pathname most likely looks like /collections/something/products/product-handle
+	    const productHandle = path.substring(path.lastIndexOf("/") + 1);
+		console.log(productHandle);
+		
+		const requestPath = "/apps/valette/product/"+productHandle+"/preorder_settings/"
+
+		$.get(requestPath)
+		  .done(function( data ) {
+
+			var stringified = JSON.stringify(data);
+		    var preorderDetails = JSON.parse(stringified);
+		    console.log( "Data Loaded: " + stringified );
+
+		    const parentDivClasses = 'product-form__item product-form__item--submit product-form__item--payment-button';
+		    const buttonStyle = 'display: block; width: 100%; font-size: 16px; font-family: font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; line-height: 1.4; padding: 8px; margin-top: 10px; margin-bottom: 10px; min-height: 44px; background-color: #000; color: #3a3a3a; border-color: #3a3a3a;';
+		    const buttonClass = 'shopify-payment-button';
+		    const message = '<p>'+preorderDetails.messageText+'</p>'
+
+//		    $('.product-form__item--payment-button').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="'+buttonClass+'" style="'+buttonStyle+'" data-testid="Checkout-button">'+preorderDetails.buttonText+'</button>'+message+'</div>')
+			
+			$('.product-form__item--payment-button').hide();
+
+			if(preorderDetails.goalReached){
+				$('.product-form__controls-group--submit').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="'+buttonClass+'" style="'+buttonStyle+'" data-testid="Checkout-button" disabled>'+preorderDetails.disabledButtonText+'</button></div>')	
+	
+			}else{
+				$('.product-form__controls-group--submit').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="'+buttonClass+'" style="'+buttonStyle+'" data-testid="Checkout-button">'+preorderDetails.buttonText+'</button>'+message+'</div>')	
+			}
+		
+
+			console.log('Button Added!');
+
+		  }).fail(function(){ 
+			  // Handle error here
+			  console.log("Not a preorder");
+		});
+
+});*/
+
+//<script>
+    
+function ready(callback){
+    // in case the document is already rendered
+    if (document.readyState!='loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function(){
+        if (document.readyState=='complete') callback();
+    });
+}
+
+ready(function(){
+    alert('It works!');
+});
+/*document.g
+$(document).ready(function(){
+   setTimeout(function(){
+      PopUp();
+   },5000); // 5000 to load it after 5 seconds from page load
+});
+
+function PopUp(){
+    document.getElementById('ac-wrapper').style.display="none"; 
+}*/
+//</script>
