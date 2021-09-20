99c011efaeb4e5701e77dae09cc2d3bd0db59705
tjark002 <tjarkh@arcor.de>
2021-09-20 13:21:43 +0200

    Update popupScriptTag.js

--- popupScriptTag.js
+++ popupScriptTag.js
@@ -49,6 +49,7 @@ style_rules.push(".formPopup" + " { display: none;position: fixed;left: 45%;top:
 style_rules.push(".formContainer" + " { max-width: 300px;padding: 20px;background-color: #fff; border-radius:7px} ");
 style_rules.push("#resellerid" + " { background-color: white;color: #333333; border-color: #d9d9d9; border: 1px solid; background-clip: padding-box; border-radius: 5px; display: block; -webkit-box-sizing: border-box; box-sizing: border-box; width: 100%; padding: 0.9285714286em 0.7857142857em; word-break: normal; line-height: inherit; } ");
 style_rules.push(".btn" + " { cursor: pointer; display: inline; margin: 0 auto; justify-content: center; align-items: center; box-sizing: border-box; font: inherit; text-decoration: none; border: 0; border-radius: 0; background-color: var(--color-button-background); box-shadow: 0 0 0 .1rem var(--color-button-border); color: var(--color-button-text); min-width: 12rem; min-height: 4.5rem; transition: box-shadow var(--duration-short) ease; -webkit-appearance: none; appearance: none; } ");
+style_rules.push("#flex-btns" + "{ display: flex; justify-content: space-between; } ");
 
 
 var style = document.createElement('style');
@@ -77,7 +78,7 @@ ready(function(){
     var formPopup = document.createElement('div');
     formPopup.setAttribute("class", "formPopup");
     formPopup.setAttribute("id", "popupForm");
-    formPopup.innerHTML = "<div class='formContainer'><h2>Bitte gebe Deine Reseller-ID ein:</h2><label for='email'>  <strong>Reseller-ID</strong></label><input type='text' id='resellerid' placeholder='Die ID Deines Resellers' name='resellerid' required></input><button onclick='sendResellerIdToBackend(0)' class='btn'>Ich habe keine</button><button onclick='sendResellerIdToBackend()' class='btn'>Bestätigen</button></div>";
+    formPopup.innerHTML = "<div class='formContainer'><h2>Bitte gebe Deine Reseller-ID ein:</h2><input type='text' id='resellerid' placeholder='Die ID Deines Resellers' name='resellerid' required></input><div id='flex-btns'><button onclick='sendResellerIdToBackend(0)' class='btn'>Ich habe keine</button><button onclick='sendResellerIdToBackend()' class='btn'>Bestätigen</button></div></div>";
     
     document.body.appendChild(loginPopup);
     document.getElementsByClassName('loginPopup')[0].appendChild(formPopup);
